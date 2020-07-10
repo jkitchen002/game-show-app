c5ab0d828795e20b397a0d0876eabdb9214da6e3
jkitchen002 <joseff.kitchen@gmail.com>
2020-07-10 15:29:20 +0400

    Amended letterFound IF statement to remove a life.

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@ const startGame = document.querySelector(".btn__reset");
 const show = document.getElementsByClassName('show');
 const qwerty =document.getElementById('qwerty');
 const button = document.getElementsByTagName('button');
+const tries = document.getElementsByClassName('tries');
+let life = 5;
+
+
 
 const phrases = [
-    "LearnToCode",
-    "JobReady",
-    "LoveCode",
-    "JobPlease",
-    "HireMe"
+    "Learn To Code",
+    "Job Ready",
+    "Love Code",
+    "Job Please",
+    "Hire Me"
 ];
 
 //click button to start game
@@ -28,12 +32,14 @@ const randomPhrase = RandomPhrase()
 
 //adds the letters of a string to the display
 function addPhrasetoDisplay () {
-    for (let i = 0; i < randomPhrase.length; i++){
+    for (let i = 0; i < randomPhrase.length; i++){ 
         const li = document.createElement('li');
         li.appendChild(document.createTextNode(randomPhrase[i]));
-        if(randomPhrase[i]){
+        if(randomPhrase[i] == " ") {
+        li.setAttribute('class', 'space');
+    } else {
         li.setAttribute('class', 'letter');
-    };
+    }
     const ul = document.querySelector('#phrase ul');
     ul.appendChild(li);
     };
@@ -45,13 +51,30 @@ function checkLetter(button) {
     let match = null;
     for (let i = 0; i < li.length; i++) {
         if (button.textContent.toUpperCase() == li[i].textContent.toUpperCase()) {
-            li[i].classList.add('show');
+            li[i].className += 'show';
             match = button.textContent;
-        }
-    }
+        };
+    };
     return match;
-}
+};
 
-qwerty.addEventListener('click', (e) => {
-      e.target.className += 'chosen';
-  });
+//listen for button clicks
+qwerty.addEventListener('click', e => {
+    if (e.target.tagName == 'BUTTON') {
+        const button = e.target;
+        button.className += 'chosen';
+        button.disabled = true;
+        const letterFound = checkLetter(button);
+    
+    if (letterFound == null) {
+        for (let i = 0; i <tries.length; i--) {
+            tries[i].style.display = 'none';
+            console.log('Nothing found')
+ 
+    else { button.classList.add('show')
+            button.classList.remove('chosen')
+        } // line 74
+    } // line 70 for loop
+    }; // line 69 
+    }; // line 63 
+}); // line 62 Event Listener
